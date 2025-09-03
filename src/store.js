0e8ad03c4d032fd57fb7c00834667dad20ee0690
Mark Steward Bayot <marksteward08@gmail.com>
2025-09-03 21:40:26 +0800

    Add Redux for state management and implement account operations (createStore)

--- src/store.js
+++ src/store.js
@@ -1,3 +1,5 @@
+import { createStore } from "redux";
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -18,10 +20,14 @@ function reducer(state = initialState, action) {
       };
     case "account/requestLoan":
       if (state.loan > 0) return state;
+
+      const { amount, purpose } = action.payload;
+
       return {
         ...state,
-        loan: state.loan + action.payload.purpose, // will modify later
-        loanPurpose: action.purpose,
+        loan: state.loan + amount,
+        loanPurpose: purpose,
+        balance: state.balance + amount,
       };
     case "account/repayLoan":
       if (state.loan === 0) return state;
@@ -35,3 +41,26 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+
+const store = createStore(reducer);
+
+store.dispatch({ type: "account/deposit", payload: 100 });
+
+console.log(store.getState());
+
+store.dispatch({ type: "account/withdraw", payload: 50 });
+
+console.log(store.getState());
+
+store.dispatch({
+  type: "account/requestLoan",
+  payload: { amount: 100, purpose: "Home Renovation" },
+});
+
+console.log(store.getState());
+
+store.dispatch({ type: "account/repayLoan" });
+
+console.log(store.getState());
+
+// action creators
