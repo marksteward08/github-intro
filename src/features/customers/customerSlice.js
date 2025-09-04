786ae453ce3aee9637bcd79cedb4742ef9f8dd99
Mark Steward Bayot <marksteward08@gmail.com>
2025-09-04 22:37:57 +0800

    Refactor customer slice to use Redux Toolkit and implement createCustomer reducer

--- src/features/customers/customerSlice.js
+++ src/features/customers/customerSlice.js
@@ -1,14 +1,4 @@
-export default function customerReducer(state = initialStateCustomer, action) {
-  switch (action.type) {
-    case "customer/create":
-      return {
-        ...state,
-        ...action.payload,
-      };
-    default:
-      return state;
-  }
-}
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialStateCustomer = {
   fullName: "",
@@ -16,13 +6,50 @@ const initialStateCustomer = {
   createdAt: null,
 };
 
-export function createCustomer(fullName, nationalID) {
-  return {
-    type: "customer/create",
-    payload: {
-      fullName,
-      nationalID,
-      createdAt: new Date().toISOString().slice(0, 10),
+const customerSlice = createSlice({
+  name: "customer",
+  initialState: initialStateCustomer,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalID) {
+        return {
+          payload: {
+            fullName,
+            nationalID,
+          },
+        };
+      },
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalID = action.payload.nationalID;
+        state.createdAt = new Date().toISOString().slice(0, 10);
+      },
     },
-  };
-}
+  },
+});
+
+export const { createCustomer } = customerSlice.actions;
+export default customerSlice.reducer;
+
+// export default function customerReducer(state = initialStateCustomer, action) {
+//   switch (action.type) {
+//     case "customer/create":
+//       return {
+//         ...state,
+//         ...action.payload,
+//       };
+//     default:
+//       return state;
+//   }
+// }
+
+// export function createCustomer(fullName, nationalID) {
+//   return {
+//     type: "customer/create",
+//     payload: {
+//       fullName,
+//       nationalID,
+//       createdAt: new Date().toISOString().slice(0, 10),
+//     },
+//   };
+// }
