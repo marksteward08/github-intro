65ac631bec2a4c48860fbc748ec32484a64d0d09
Mark Steward Bayot <marksteward08@gmail.com>
2025-09-04 20:43:23 +0800

    Migrated from Old Redux to Redux Toolkit

--- src/store.js
+++ src/store.js
@@ -1,13 +1,13 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
 import customerReducer from "./features/customers/customerSlice";
 import bankReducer from "./features/accounts/accountSlice";
-import { thunk } from "redux-thunk";
 
-const rootReducer = combineReducers({
-  bank: bankReducer,
-  customer: customerReducer,
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer: {
+    bank: bankReducer,
+    customer: customerReducer,
+  },
 });
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
